be1aa1527031b1c5d438ffaeb7e62d4f9eb6b2e6
M. A. Williams <mwilliamsii233203@gmail.com>
2018-07-24 10:52:09 -0400

    Add files via upload

---
+++ home.js
@@ -0,0 +1,9 @@
+(function(){
+    $(document).ready(init);
+
+    function init(){
+        // $("#navSlide").on('click', navSlide);
+        $(".dropdown-trigger").dropdown();
+        $('.sidenav').sidenav();
+    }
+})();
